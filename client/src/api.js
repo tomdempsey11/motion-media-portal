581aa2e19858fa3c95504cddfeb039e254c53064
Tom Dempsey <tomdempsey@Toms-Air.lan>
2025-12-17 22:50:18 -0800

    hopefully final push

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001/api", // ✅ include /api
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5001/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
